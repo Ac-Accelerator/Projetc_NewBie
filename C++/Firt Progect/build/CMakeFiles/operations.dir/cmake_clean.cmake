89c2cee22aa0217294b97bc2927bdc9e2a87fd6a
AC_Accelerator <dengyuzhe.personal@gmail.com>
2021-12-01 16:23:44 +0800

    update

---
+++ C++/Firt Progect/build/CMakeFiles/operations.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/operations.dir/src/function.obj"
+  "CMakeFiles/operations.dir/src/function.obj.d"
+  "CMakeFiles/operations.dir/src/goods.obj"
+  "CMakeFiles/operations.dir/src/goods.obj.d"
+  "liboperations.dll"
+  "liboperations.dll.a"
+  "liboperations.dll.manifest"
+  "liboperations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/operations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
