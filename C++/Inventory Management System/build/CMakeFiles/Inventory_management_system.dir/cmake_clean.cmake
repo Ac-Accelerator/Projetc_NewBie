75d76ce45183b36b282d2d460bcb05b5747d425f
AC_Accelerator <dengyuzhe.personal@gmail.com>
2021-12-05 21:43:02 +0800

    update

---
+++ C++/Inventory Management System/build/CMakeFiles/Inventory_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Inventory_management_system.dir/src/main.obj"
+  "CMakeFiles/Inventory_management_system.dir/src/main.obj.d"
+  "Inventory_management_system.exe"
+  "Inventory_management_system.exe.manifest"
+  "Inventory_management_system.pdb"
+  "libInventory_management_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Inventory_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/Inventory Management System/build/CMakeFiles/operations.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/My Programs/Projetc_NewBie/C++/Inventory Management System/src/function.cpp" "CMakeFiles/operations.dir/src/function.obj" "gcc" "CMakeFiles/operations.dir/src/function.obj.d"
+  "E:/My Programs/Projetc_NewBie/C++/Inventory Management System/src/goods.cpp" "CMakeFiles/operations.dir/src/goods.obj" "gcc" "CMakeFiles/operations.dir/src/goods.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
